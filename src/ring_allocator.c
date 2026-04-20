c2d7c7781086092a5b3101b06b2c48728f347de2
Banana-4 <stig34768@gmail.com>
2026-04-20 13:05:29 +0000

    Refactor: move last to file scopeadd ring_free function
    move the last allocated block to the file scope,

--- src/ring_allocator.c
+++ src/ring_allocator.c
@@ -1,9 +1,8 @@
 #include "../include/ring_allocator.h"
 #include "../include/internal.h"
-
+static Header *last = NULL;
 void *ring_alloc(size_t n) {
   Header *p, *q;
-  static Header *last = NULL;
   if (!last) {
     last = morecore(n);
   }
@@ -32,6 +31,32 @@ void *ring_alloc(size_t n) {
       ring_free(last);
     }
   }
-
+void ring_free(Header *ptr) {
+  if (!ptr) {
+    return;
+  }
+  Header *q, *p;
+  for (p = last, q = last->metadata.next;;
+       p = p->metadata.next, q = q->metadata.next) {
+    if (p < ptr && q > ptr) {
+      if (((void *)p + p->metadata.size) == ptr) {
+        p->metadata.size += ptr->metadata.size;
+        ptr = p;
+      }
+      if (((void *)ptr + ptr->metadata.size) == q) {
+        ptr->metadata.size += q->metadata.size;
+        ptr->metadata.next = q->metadata.next;
+        q = ptr;
+      }
+      if (ptr != p) {
+        p->metadata.next = ptr;
+      }
+      if (q != ptr) {
+        ptr->metadata.next = q;
+      }
+      break;
+    }
+  }
+}
   return q + 1;
 }
