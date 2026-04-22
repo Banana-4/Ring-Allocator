a5476c6d68d060d6601a8a42227a4b7e02791aff
Banana-4 <stig34768@gmail.com>
2026-04-22 14:40:38 +0000

    rework: change Header to struct, add align_up()

--- include/ring_allocator.h
+++ include/ring_allocator.h
@@ -1,14 +1,11 @@
 #pragma once
+#include <stdalign.h>
+#include <stddef.h>
 #include <stdlib.h>
 
-typedef int ALIGN;
-
-typedef union Header {
-  struct {
-    union Header *next;
+typedef struct Header {
+    struct Header *next;
     size_t size;
-  } metadata;
-  ALIGN x;
 } Header;
 
 void *ring_alloc(size_t n);
