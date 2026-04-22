b6c90d713f9ae4cf67c8206088af7ade6de4e15a
Banana-4 <stig34768@gmail.com>
2026-04-22 14:41:28 +0000

    reworK: change Header to struct, use align_up()

--- src/ring_allocator.c
+++ src/ring_allocator.c
@@ -1,30 +1,39 @@
 #include "../include/ring_allocator.h"
 #include "../include/internal.h"
 
+inline size_t align_up(size_t size) {
+    return (size + alignof(max_align_t) - 1) & ~(alignof(max_align_t) - 1);
+}
+
+
 static Header *last = NULL;
 
 void *ring_alloc(size_t n) {
+    if (n == 0)
+        return NULL;
+
   Header *p, *q;
-  n += sizeof(Header);
+  n = align_up(n + sizeof(Header));
   if (!last) {
-    last = morecore(n);
+      last = morecore(n >= BLOCKSIZE ? n : align_up(n + BLOCKSIZE) );
   }
 
-  for (p = last, q = p->metadata.next;;
-       p = p->metadata.next, q = q->metadata.next) {
-    if (q->metadata.size >= n) {
-      if (q->metadata.size == n) {
-        p->metadata.next = q->metadata.next;
+  for (p = last, q = p->next;;
+       p = p->next, q = q->next) {
+    if (q->size >= n) {
+      if (q->size == n) {
+        p->next = q->next;
+        q->size -= n;
       } else {
-        q->metadata.size -= n;
-        q = (Header *)((char *)q + q->metadata.size);
-        q->metadata.next = NULL;
-        q->metadata.size = n;
+        q->size -= n;
+        q = (Header *)((char *)q + q->size);
+        q->next = NULL;
+        q->size = n;
       }
       break;
     }
     if (q == last) {
-      Header *new = morecore(n);
+      Header *new = morecore(n >= BLOCKSIZE ? n : align_up(n + BLOCKSIZE) );
       if (!new) {
         return NULL;
       }
@@ -40,24 +49,24 @@ void ring_free(Header *ptr) {
     return;
   }
   Header *q, *p;
-  for (p = last, q = last->metadata.next;;
-       p = p->metadata.next, q = q->metadata.next) {
+  for (p = last, q = last->next;;
+       p = p->next, q = q->next) {
     if (p <= ptr && q >= ptr) {
-      if (((char *)p + p->metadata.size) == (char *)ptr) {
-        p->metadata.size += ptr->metadata.size;
+      if (((char *)p + p->size) == (char *)ptr) {
+        p->size += ptr->size;
         ptr = p;
       }
-      if ((char *)q == (char *)ptr + ptr->metadata.size) {
-        ptr->metadata.size += q->metadata.size;
-        ptr->metadata.next = q->metadata.next;
+      if ((char *)q == (char *)ptr + ptr->size) {
+        ptr->size += q->size;
+        ptr->next = q->next;
         if (ptr != p) {
-          p->metadata.next = ptr;
+          p->next = ptr;
         }
       }
 
-      if (p->metadata.next != ptr && ptr != p) {
-        p->metadata.next = ptr;
-        ptr->metadata.next = q;
+      if (p->next != ptr && ptr != p) {
+        p->next = ptr;
+        ptr->next = q;
       }
       break;
     }
