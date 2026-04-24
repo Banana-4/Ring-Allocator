38edd9fe9745019206291b2708e0b4e9e99ebd5f
Banana-4 <stig34768@gmail.com>
2026-04-24 20:29:25 +0000

    fix: alloc and free

--- src/ring_allocator.c
+++ src/ring_allocator.c
@@ -1,7 +1,8 @@
 #include "../include/ring_allocator.h"
 #include "../include/internal.h"
 
-inline size_t align_up(size_t size) {
+
+ size_t align_up(size_t size) {
     return (size + alignof(max_align_t) - 1) & ~(alignof(max_align_t) - 1);
 }
 
@@ -15,7 +16,10 @@ void *ring_alloc(size_t n) {
   Header *p, *q;
   n = align_up(n + sizeof(Header));
   if (!last) {
-      last = morecore(n >= BLOCKSIZE ? n : align_up(n + BLOCKSIZE) );
+      last = morecore(align_up(n + BLOCKSIZE));
+      if (!last) {
+          return NULL;
+      }
   }
 
   for (p = last, q = p->next;;
@@ -25,50 +29,56 @@ void *ring_alloc(size_t n) {
         p->next = q->next;
         q->size -= n;
       } else {
-        q->size -= n;
-        q = (Header *)((char *)q + q->size);
-        q->next = NULL;
-        q->size = n;
+          q->size -= n;
+          q = (Header *)((char *)q + q->size);
+          q->size = n;
       }
       break;
     }
     if (q == last) {
-      Header *new = morecore(n >= BLOCKSIZE ? n : align_up(n + BLOCKSIZE) );
+      Header *new = morecore(align_up(n + BLOCKSIZE));
       if (!new) {
         return NULL;
       }
+      ring_free(new + 1);
       last = new;
-      ring_free(last);
     }
   }
   return (void *)(q + 1);
 }
 
-void ring_free(Header *ptr) {
-  if (!ptr) {
-    return;
-  }
-  Header *q, *p;
-  for (p = last, q = last->next;;
-       p = p->next, q = q->next) {
-    if (p <= ptr && q >= ptr) {
-      if (((char *)p + p->size) == (char *)ptr) {
-        p->size += ptr->size;
-        ptr = p;
-      }
-      if ((char *)q == (char *)ptr + ptr->size) {
-        ptr->size += q->size;
-        ptr->next = q->next;
-        if (ptr != p) {
-          p->next = ptr;
-        }
-      }
-
-      if (p->next != ptr && ptr != p) {
-        p->next = ptr;
-        ptr->next = q;
-      }
-      break;
+void ring_free(void *ptr) {
+    if (!ptr) {
+        return;
     }
-  }
+    Header *block = (Header *)ptr - 1;
+    Header *p = last;
+
+    for (; !(block > p && block < p->next); p = p->next) {
+        if (p >= p->next && (p > block || block < p->next)) {
+            break;
+        }
+    }
+    if ((char *)p + p->size == (char *)block) {
+        p += block->size;
+        block = p;
+    }
+    if ((char *)p->next == (char *)block + block->size) {
+        block->size += p->next->size;
+        block->next = p->next->next;
+    } else {
+        block->next = p->next;
+        p->next = block;
+    }
+}
+
+void print_debug() {
+    Header *p = last;
+    while(p) {
+        printf("[ Block: %p | Size: %zu | Next: %p ]\n", p, p->size, p->next);
+        p = p->next;
+        if (p == last) {
+            break;
+        }
+    }
 }
